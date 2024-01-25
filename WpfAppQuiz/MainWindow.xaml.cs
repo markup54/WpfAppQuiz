aa75524f193cff458c3abd2928147e3d5b32e6bf
markup54 <marzenakupka@tlen.pl>
2024-01-25 09:13:20 +0100

    Dodaj pliki projektów.

---
+++ WpfAppQuiz/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppQuiz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Pytanie> PytanieList { get; set; }
+        public int Aktualne {get;set;}
+        public MainWindow()
+        {
+            InitializeComponent();
+            przygotujPytania();
+            Aktualne = 0;
+            wyswietlPytanie();
+        }
+        private void wyswietlPytanie()
+        {
+            tresc_txt.Text = PytanieList[Aktualne].TrescPytania;
+        }
+        private void przygotujPytania()
+        {
+            PytanieList = new List<Pytanie>();
+            PytanieList.Add(new Pytanie("Czy programista zdaje egzamin inf04?", true));
+            PytanieList.Add(new Pytanie("Czy programista zdaje egzamin inf02?", false));
+            PytanieList.Add(new Pytanie("Czy programista powinien znać się na sieciach komputerowych?", true));
+        }
+
+        private void Button_Click_Nastepne(object sender, RoutedEventArgs e)
+        {
+            Aktualne++;
+            wyswietlPytanie();
+        }
+    }
+}
---
+++ WpfAppQuiz/Pytanie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppQuiz
+{
+   public class Pytanie
+    {
+        public string TrescPytania { get; set; }
+        /*private string _trescPytania;
+        public string TrescPytania
+        {
+            //get { return _trescPytania;}
+            get => _trescPytania;
+            //set => _trescPytania = value;
+            set
+            {
+                _trescPytania = value;
+            }
+        }*/
+        public bool PoprawnaOdpowiedz { get; set; }
+        public bool UdzielonaOdpowiedz { get; set; }
+        public bool CzyUdzielonaOdpowiedz { get; set; }
+
+        public Pytanie(string trescPytania, bool poprawnaOdpowiedz)
+        {
+            TrescPytania = trescPytania;
+            PoprawnaOdpowiedz = poprawnaOdpowiedz;
+            UdzielonaOdpowiedz = !PoprawnaOdpowiedz;
+            CzyUdzielonaOdpowiedz = false;
+        }
+    }
+}
